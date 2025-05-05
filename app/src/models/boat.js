c95ec1fc900bd27a7e273517c14b25e0fb3acf7a
Alex Green <alex.green@itas.ca>
2025-05-05 15:26:02 -0700

    boats have a number instead of having to have many relations

--- app/src/models/boat.js
+++ app/src/models/boat.js
@@ -1,7 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
     const Boat = sequelize.define('Boat', {
       type: DataTypes.STRING,           // canoe or kayak
-      isRented: DataTypes.BOOLEAN       // true or false (just for data / cannot end in the wrong place e.g. as the campers leave, the rented boat leaves with them (for much later))
+      isRented: DataTypes.BOOLEAN,      // true or false (just for data / cannot end in the wrong place e.g. as the campers leave, the rented boat leaves with them (for much later))
+      numberOf: DataTypes.INTEGER       // the number if this type of boat used
     });
   
     Boat.associate = (models) => {  
