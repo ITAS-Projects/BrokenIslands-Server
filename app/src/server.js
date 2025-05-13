da8bb30aa676f9582b1cf673dd32dcdbf73faaa0
Alex Green <alex.green@itas.ca>
2025-05-13 12:19:14 -0700

    added api to routes to work on same site

--- app/src/server.js
+++ app/src/server.js
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/people', require('./routes/person'));
-app.use('/reservations', require('./routes/reservation'));
-app.use('/groups', require('./routes/group'));
-app.use('/taxis', require('./routes/taxi'));
-app.use('/boats', require('./routes/boat'));
-app.use('/trips', require('./routes/trip'));
+app.use('/api/people', require('./routes/person'));
+app.use('/api/reservations', require('./routes/reservation'));
+app.use('/api/groups', require('./routes/group'));
+app.use('/api/taxis', require('./routes/taxi'));
+app.use('/api/boats', require('./routes/boat'));
+app.use('/api/trips', require('./routes/trip'));
 
 db.sequelize.sync().then(() => {
   app.listen(8081, () => console.log('Server is running on http://localhost:8081'));
